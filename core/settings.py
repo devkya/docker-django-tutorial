effe7244ce430c476d25ab0792a573db4acf81a7
devkya <kihaok@gmail.com>
2023-10-13 19:22:13 +0900

    Modify pgadmin4

--- core/settings.py
+++ core/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "docker-tutorial-db",
+        "USER": "devkya",
+        "PASSWORD": "1q2w3e!Q",
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
     }
 }
 
